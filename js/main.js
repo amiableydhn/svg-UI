94761506f77734525f2a71b4360c261154c8cee2
amiableydhn <amiableydhn@naver.com>
2022-08-10 22:16:28 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const btnUp = document.querySelector('.btn_up');
+const btnDown = document.querySelector('.btn_down');
+const panel = document.querySelector('.panel');
+const panel_li = panel.querySelectorAll('li');
+const len = panel_li.length-1;
+let enableClick = true; //재클릭방지
+
+btnUp.addEventListener('click', e=>{
+    e.preventDefault();//링크 이벤트 막기
+    
+    if(enableClick){
+        enableClick = false;
+        moveUp();
+    }
+    
+
+    // let current_item = panel.querySelector('.on');
+    // let current_index = parseInt(current_item.getAttribute('data-index'));
+    // //console.log(current_index);
+    // let next_index = null;
+
+    // (current_index !== len) ? next_index = current_index+1 : next_index = 0;
+    // current_item.classList.remove('on');
+    // current_item.classList.add('up');
+
+    // panel_li[next_index].classList.add('down');
+    // setTimeout(()=>{
+    //     panel_li[next_index].classList.remove('down');
+    //     panel_li[next_index].classList.add('on');
+    //     panel.querySelector('.up').classList.remove('up');
+    // },500);
+})
+
+btnDown.addEventListener('click', e=> {
+    e.preventDefault();
+    if(enableClick){
+        enableClick = false;
+        moveDown();
+    }
+})
+
+
+function moveUp(){
+    let current_item = panel.querySelector('.on');
+    let current_index = parseInt(current_item.getAttribute('data-index'));
+    //console.log(current_index);
+    let next_index = null;
+
+    (current_index !== len) ? next_index = current_index+1 : next_index = 0;
+    current_item.classList.remove('on');
+    current_item.classList.add('up');
+
+    panel_li[next_index].classList.add('down');
+    setTimeout(()=>{
+        panel_li[next_index].classList.remove('down');
+        panel_li[next_index].classList.add('on');
+        panel.querySelector('.up').classList.remove('up');
+        enableClick = true;
+    },500);
+}
+
+function moveDown(){
+    let current_item = panel.querySelector('.on');
+    let current_index = parseInt(current_item.getAttribute('data-index'));
+    //console.log(current_index);
+    let prev_index = null;
+
+    (current_index !== 0) ? prev_index = current_index-1 : prev_index = len;
+    current_item.classList.remove('on');
+    current_item.classList.add('down');
+
+    panel_li[prev_index].classList.add('up');
+    setTimeout(()=>{
+        panel_li[prev_index].classList.remove('up');
+        panel_li[prev_index].classList.add('on');
+        panel.querySelector('.down').classList.remove('down');
+        enableClick = true;
+    },500);
+}
